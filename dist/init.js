6e1eb3b4a1d9d10e4a39e86d5fa353812c6201d7
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-25 09:44:11 -0500

    Update init.js

--- dist/init.js
+++ dist/init.js
@@ -1,9 +1,18 @@
 import VaderRouter from "vader-router";
 import { Home } from "../src/pages/Home.js";
+import { About } from "../src/pages/About.js";
 const app = new VaderRouter('/');
 app.use('/', async (req, res) => {
     console.log(req)
     res.render('#root', await Home.render());
 });
+app.on('/', async (req, res) => {
+    console.log(req)
+    res.render('#root', await Home.render());
+});
+app.use('/about', async (q,r)=>{
+    r.render('#root', await About.render())
+})
+
  
 app.start ();
