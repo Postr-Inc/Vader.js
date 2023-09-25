9926f78019d9c233fbece90e029f2fd4b2162260
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-25 08:59:37 -0500

    use Signals

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,26 @@
 import { component, rf, include, vhtml, useRef } from "vader";
 
+export const Home = component("Home", {
+  render: async () => {
+    let count = signal("count", 0);
 
-export  const Home = component('Home', {
-   
-    render: async () => {
-        let [count, setCount] = useState('count', 0)
+    let counter = count.subscribe((s) => {
+      console.log("count state updated", s);
+      return () => {
+        count.cleanup(counter);
+      };
+    }, false); // this is ran once
+    count.call();
+    count.cleanup(counter);
 
-        useEffect(() => {
-            console.log('count', count)
-        }, ['count'])
-
-        function increment() {
-            setCount(count + 1)
-        }
-        rf('increment', increment)
-        let home  = await include('/src/views/home.html')
-        return vhtml(home)
-    },
-    componentDidMount: () => {
-        console.log('componentDidMount')
-    },
-})
+    function increment() {
+      count.set(count.get() + 1);
+    }
+    rf("increment", increment);
+    let home = await include("/src/views/home.html");
+    return vhtml(home);
+  },
+  componentDidMount: () => {
+    console.log("componentDidMount");
+  },
+});
