a11e9454c26aa8be0be58f5a366562ab95e3a534
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-26 20:02:41 -0700

    fix imports

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
 /**
  * @file index.js
  * @description This is the entry point for the library. This file exports all the necessary classes and functions.
  * @version 1.1.5
- * 
+ *
  */
-import { Vader } from "./vader.js";
+import Vader, { include, useRef } from './vader.js';
 // @ts-ignore
-import { VaderRouter } from "./vader-router.js";
-
-export { Vader, VaderRouter };
+import { VaderRouter } from './vaderRouter.js';
+export { useRef, include, VaderRouter };
+export default Vader;
