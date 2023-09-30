62ce9833ba519e6d2cda3c3a3e862877fd246a2e
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-30 09:07:59 -0700

    Moved markdown parsing to worker side

--- worker.js
+++ worker.js
@@ -1,3 +1,4 @@
+ 
 onmessage = (e)=>{
     let time_started = Date.now()
     let strings =  e.data.strings
@@ -21,15 +22,59 @@ onmessage = (e)=>{
         }
     }
 
-    let code =  result.match(/<code([^>]*)>/g)
-    if(code){
-        while(code.length){
-            let c = code.pop()
-            // @ts-ignore
-            c = c.replace('<code','').replace('>','')
-            result = result.replace(`<code${c}>`,`<code${c} style="background:#000;color:#fff;padding:5px;border-radius:5px;font-size:12px;font-weight:bold">`)
-        }
-    }
+  
+    // Convert headings (e.g., #1-6 Heading => <h1-6>Heading</h1-6>)
+      result = result.replace(/(#+)(.*)/g, (match, hashes, text) => {
+        let level = hashes.length;
+        return `<h ${level} class="$markdown_heading">${text}</h${level}>`;
+      });
+  
+    
+      // Convert bold (e.g., **Bold** => <b>Bold</b>)
+      result = result.replace(/\*\*(.*?)\*\*/g, (match, text) => {
+        return `<b class="$markdown_bold">${text}</b>`;
+      });
+    
+      // Convert italic (e.g., *Italic* => <i>Italic</i>)
+      result = result.replace(/\*(.*?)\*/g, (match, text) => {
+        return `<i class="$markdown_italic">${text}</i>`;
+      });
+    
+      // Convert code (e.g., `code` => <code>code</code>)
+      result = result.replace(/`(.*?)`/g, (match, text) => {
+        return `<code>${text}</code>`;
+      });
+    
+      // Convert links (e.g., [Text](URL) => <a href="URL">Text</a>)
+      result = result.replace(/\[([^\]]+)\]\(([^)]+)\)/g, (match, text, url) => {
+        return `<a  class="$markdown_link" href="${url}">${text}</a>`;
+      });
+    
+      // Convert images (e.g., ![Alt](URL) => <img src="URL" alt="Alt" />)
+      result = result.replace(/!\[([^\]]+)\]\(([^)]+)\)/g, (match, alt, src) => {
+        return `<img  class="$markdown_image" src="${src}" alt="${alt}" />`;
+      });
+    
+      // Convert unordered lists (e.g., * Item => <ul><li>Item</li></ul>)
+      result = result.replace(/^\s*\*\s+(.*?)$/gm, (match, text) => {
+        return `<ul class="$markdown_unordered"><li>${text}</li></ul>`;
+      });
+    
+      // Convert ordered lists (e.g., 1. Item => <ol><li>Item</li></ol>)
+      result = result.replace(/^\s*\d+\.\s+(.*?)$/gm, (match, text) => {
+        return `<ol class="$markdown_ordered"><li>${text}</li></ol>`;
+      });
+    
+      // Convert list items (e.g., - Item => <li>Item</li>)
+      result = result.replace(/^\s*-\s+(.*?)$/gm, (match, text) => {
+        return `<li class="$markdown_list_item">${text}</li>`;
+      });
+    
+      // Convert horizontal rules (e.g., --- => <hr />)
+      result = result.replace(/^\s*---\s*$/gm, '<hr class="$markdown_horizontal" />');
+    
+      
+   
  
  
     if(!result.includes('<body>')){
@@ -47,7 +92,8 @@ onmessage = (e)=>{
      */
     // @ts-ignore
     let images = result.match(/<img([^>]*)>/g)
-    for(let i = 0; i < images.length; i++){
+     if(images){
+        for(let i = 0; i < images.length; i++){
       let image = images[i]
       let src = image.match(/src="([^"]*)"/)
       let alt = image.match(/alt="([^"]*)"/)
@@ -77,6 +123,7 @@ onmessage = (e)=>{
             console.log('already cached', caches.match(`${l}/public/${src[1]}`))
        }
     }
+     }
 
     let href =  result.match(/href="([^"]*)"/g)
     if(href){
