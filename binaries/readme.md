99bfcf9213718e988510b48d54796bef40aa92a3
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-02-25 07:40:21 -0600

    Delete binaries directory

--- binaries/readme.md
+++
@@ -1,4 +0,0 @@
-#Binaries
-
-Folder contains polyfills for missing functionality within windows
-Folder contains ssg code for generating html pages for each route on the fly
--- binaries/win32/check.ps1
+++
@@ -1,7 +0,0 @@
-$chrometest = Test-Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe'
-
-if($chrometest -eq $true){
-   Write-Host "Google Chrome is installed"
-}else{
-   Write-Host "Google Chrome is not installed"
-}
