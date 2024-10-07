e6b04a4b353666a34878d093ef9c6369559c7340
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-10-07 15:20:44 -0700

    Update tailwind.ts

--- plugins/tailwind.ts
+++ plugins/tailwind.ts
@@ -13,7 +13,15 @@ function checkIfTailwindInstalled() {
 
 function initTailwind() { 
     const tailwindConfig = path.resolve(process.cwd(), 'tailwind.config.js')
+    const postcssConfig = path.resolve(process.cwd(), 'postcss.config.js')
     if (!fs.existsSync(tailwindConfig)) {
+        fs.writeFileSync(postcssConfig, `module.exports = {
+            plugins: {
+              tailwindcss: {},  
+              autoprefixer: {},
+              }
+              }`)
+
         fs.writeFileSync(tailwindConfig, `/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}', './app/**/*.{html,js,jsx,ts,tsx}'],
@@ -32,19 +40,25 @@ export default {
     name: 'tailwindcss',
     description: 'TailwindCSS plugin for Vader.js',
     version: '0.0.1',
-    onBuildFinish: async (vader) => {
+    onBuildStart: async (vader) => {
         if (!checkIfTailwindInstalled()) {
-            console.error('TailwindCSS is not installed. Please install it using `bun install tailwindcss`')
+            console.error('TailwindCSS is not installed. Please install it using `bun install  tailwindcss postcss-cli autoprefixer`')
             process.exit(1)
         }else{
            initTailwind()
+           
            vader.onFileChange('tailwind.config.js', async () => {
                 console.log('Rebuilding TailwindCSS...')
-                await vader.runCommand(['bun', 'run', 'tailwindcss', 'build', '-o', 'public/styles.css'])
+                await vader.runCommand(['bun', 'run', 'postcss',  './public/styles.css', '-o', 'dist/public/tailwind.css'])
                 console.log('TailwindCSS rebuilt successfully!')
            }) 
-           vader.runCommand(['bun', 'run', 'tailwindcss', 'build', '-o', 'public/styles.css'])
-        }
+           await vader.runCommand(['bun', 'run', 'postcss', './public/styles.css', '-o', 'dist/public/tailwind.css']) 
+           vader.injectHTML(`<style>${fs.readFileSync(path.resolve(process.cwd(), 'dist/public/tailwind.css'))}</style>`)
+        }  
+        
+    },
+    onBuildFinish: async (vader) => {
+        console.log('TailwindCSS plugin finished building')
     },
 
 }
